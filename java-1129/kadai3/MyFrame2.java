e71cf36bfb75c708b4ab6c2c18529aeeaace6c1e
sinohiro <sinohiro@momo-mail.com>
2019-12-02 16:19:55 +0900

    java-1129_kadai_add

--- java-1129/kadai3/MyFrame2.java
+++ java-1129/kadai3/MyFrame2.java
@@ -4,35 +4,82 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.io.*;
 
-public class MyFrame2 extends JFrame{
+public class MyFrame2 extends JFrame implements ChangeListener {
 	private MyPanel2 mp;// MyFrame2 HAS-A MyPanel の関係を構築
-	private JButton accel;
-	private JSLider boosts;
-	private JLabel	boostl;
+	private JSlider accels;
+	private JLabel	accell;
+	private JSlider brakes;
+	private JLabel	brakel;
 
 	public MyFrame2() {
 		super();
-		this.mp = new MyPanel();
-		this.accel = new JButton("Accel");
-		this.boosts = new JSlider();
-		this.bootsl = new JLabel();
+		this.mp = new MyPanel2();
+		this.accels = new JSlider(0, 100, 0);
+		this.accell = new JLabel();
+		this.brakes = new JSlider();
+		this.brakel = new JLabel();
+		accell.setText("accel: " + accels.getValue());
+		brakel.setText("Tako: " + this.mp.getTako());
 
-		JPanel panel1 = new JPanel();
-		panel1.setLayout(new FlowLayout());
-		panel1.add(this.mp);
-		panel1.add(this.button2);
-		panel1.add(this.err);
+		JPanel accel = new JPanel();
+		accel.add(this.accels);
+		accel.add(this.accell);
 
-		getContentPane().add(panel1);
-		this.accel.addActionListener(this);
-		this.boots.addChangeListener(this);
+		JPanel brake = new JPanel();
+		brake.add(this.brakes);
+		brake.add(this.brakel);
+
+		JPanel pedal = new JPanel();
+		pedal.setLayout(new BoxLayout(pedal, BoxLayout.PAGE_AXIS));
+		pedal.add(accel);
+		pedal.add(brake);
+
+		JPanel meter = new JPanel();
+		meter.setLayout(new BoxLayout(meter, BoxLayout.PAGE_AXIS));
+		meter.add(this.mp);
+		meter.add(pedal);
+
+		getContentPane().add(meter);
+		this.accels.addChangeListener(this);
+		//this.boots.addChangeListener(this);
 	}
+
+	public int AtoT(int n) {
+		int ans = n;
+		if (n >= 45){
+			ans = n -20;
+			ans = AtoT(ans);
+		}
+		return(ans);
+	}
+
+	public void stateChanged(ChangeEvent e) {
+		if (e.getSource() == this.accels) {
+			this.mp.setAccel(this.accels.getValue());
+			accell.setText("accel: " + accels.getValue());
+			this.mp.setTako(AtoT(this.accels.getValue()));
+			brakel.setText("Tako: " + this.mp.getTako());
+
+			if (this.mp.getTako() >= 180) {
+				this.mp.setTako(this.accels.getValue() - 20);
+				brakel.setText("Tako: " + this.mp.getTako());
+			}
+		}
+
+		if (e.getSource() == this.brakes) {
+			brakel.setText("brake: " + brakes.getValue());
+		}
+	this.mp.repaint();
+	}
+
+//	public void actionPerformed(ActionEvent ae) {
+//		if (ae.getSource() == this.up) {
 
 	public static void main(String[] args){
 		JFrame mainFrame = new JFrame("4J_21_shinodaApp");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		MyFrame2 a = new MyFrame2();
-		a.setTitle("4J_21_shinodaApp_MyTextEditer_Progrram");
+		a.setTitle("4J_21_shinodaApp");
 		a.setSize(1280, 720);
 		a.setLocation(100, 100);
 		a.setVisible(true);
--- java-1129/kadai3/MyPanel2.java
+++ java-1129/kadai3/MyPanel2.java
@@ -4,16 +4,37 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.io.*;
 
-public class MyPanel extends JPanel {
-	public MyPanel() {
+public class MyPanel2 extends JPanel {
+
+	private int accel = 0;
+	private int tako = 10;
+
+	public void setAccel(int accel){
+		this.accel = 2 * accel;
+	}
+
+	public int getAccel(){
+		return this.accel;
+	}
+
+	public void setTako(int tako){
+		this.tako = 4 * tako;
+	}
+
+	public int getTako(){
+		return this.tako;
+	}
+
+	public MyPanel2() {
+		super();
 		setBackground(Color.white); // 背景を白に設定
 		setPreferredSize(new Dimension(640, 480)); // 大きさを640x480に設定
 	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g); // まずJPanelのデフォルトの描画処理(背景色でクリア)を行い,
-		g.drawLine(50, 50, 100, 100); // その後, 独自の図形を描画する
-		g.drawOval(32, 16, 256, 128);
-		g.drawString("文字列をグラフィックスとして表示", 48, 48);
+		g.fillArc(650, 100, 500, 500, 200, -accel);
+		g.fillArc(100, 100, 500, 500, 200, -tako);
+		g.drawString("0km/h", 600, 450);
 	}
 }
 
