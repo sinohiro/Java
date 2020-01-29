22689609c9f4cb64cfe6272f8041f8ed1f9ba7b5
sinohiro <sinohiro@momo-mail.com>
2020-01-29 12:23:26 +0900

    update 0129

--- java-0122/MyApplet.java
+++ java-0122/MyApplet.java
@@ -1,23 +1,37 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.MediaTracker;
 import java.net.*;
 
-public class MyApplet extends JApplet implements KeyListener {
+public class MyApplet extends JApplet implements KeyListener, ActionListener {
 	private MyPanel mp;
 	private MyModel mm;
 	private Image player;
-	private int px, py;
+	private Timer timer;
+	private int px, py, ex, ey;
 	private boolean kleft;
 	private boolean kright;
+	private MediaTracker tracker;
 
 	public void init() {
 		px = 640;
 		py = 600;
-		kleft = kright = false;
+		ex = 20;
+		ey = 20;
+		this.kleft = this.kright = false;
+		timer = new Timer(60, this);
 		this.mm = new MyModel();
 		this.mp = new MyPanel(px, py);
+		tracker = new MediaTracker();
+
+		//mediatracker
+		try {
+			this.tracker.waitForAll();
+			System.out.println("MediaTracker_ok");
+		}catch (InterruptedException e) {
+		}
 
 		mp.addKeyListener(this);
 
@@ -25,19 +39,26 @@ public class MyApplet extends JApplet implements KeyListener {
 		field.setLayout(new BoxLayout(field, BoxLayout.PAGE_AXIS));
 		field.add(this.mp);
 
+		this.timer.start();
 		getContentPane().add(field);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		ex = mm.getEmove(false, ex);
+		mp.setEnemyx(ex);
+		this.mp.repaint();
 	}
 
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
 			case KeyEvent.VK_A:
 				System.out.println("px = " + px);
-				px = mm.getLeftmove(true, px);
+				px = mm.getLeftPmove(true, px);
 				mp.setPlayerx(px);
 				break;
 			case KeyEvent.VK_D:
 				System.out.println("px = " + px);
-				px = mm.getRightmove(true, px);
+				px = mm.getRightPmove(true, px);
 				mp.setPlayerx(px);
 				break;
 		}
@@ -47,11 +68,11 @@ public class MyApplet extends JApplet implements KeyListener {
 	public void keyReleased(KeyEvent e) {
 		switch (e.getKeyCode()) {
 			case KeyEvent.VK_A:
-				px = mm.getLeftmove(false, px);
+				px = mm.getLeftPmove(false, px);
 				mp.setPlayerx(px);
 				break;
 			case KeyEvent.VK_D:
-				px = mm.getRightmove(false, px);
+				px = mm.getRightPmove(false, px);
 				mp.setPlayerx(px);
 				break;
 		}
--- java-0122/MyModel.java
+++ java-0122/MyModel.java
@@ -5,51 +5,69 @@ import java.awt.Dimension;
 import java.net.*;
 
 public class MyModel {
-	private MyPanel mp;
-	private boolean kleft;
+	private boolean kleft;  //keyLeft
 	private boolean kright;
+	private boolean ERight; //EnemyRight
+	private boolean ELeft;
+	private boolean enemykill;
 	private Image player;
 	private Image enemy;
 
 	public MyModel(){
-		//URL url = MyModel.class.getResource("man0.png");
-		//player = super.getImage(url);
 		super();
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		player = tk.getImage("man0.png");
+		this.tracker.addImage(player, 0);
 		enemy = tk.getImage("man1.png");
-	}
+		this.tracker.addImage(enemy, 0);
+		ELeft = false;
+		ERight = true;
+		enemykill = false;
 
-	public int getLeftmove(boolean kleft, int px){
+			}
+
+	public int getLeftPmove(boolean kleft, int px){
 		if (kleft && px > 0){
-			px -= 8;
+			px -= 10;
 		}
 		return px;
 	}
 
-	public int getRightmove(boolean kright, int px){
+	public int getRightPmove(boolean kright, int px){
 		if (kright && px < 1280){
-			px += 8;
+			px += 10;
 		}
 		return px;
 	}
 
-	public int getRightmove(boolean kright, int px){
-		if (kright && px < 1280){
-			px += 8;
+	public int getEmove(boolean enemykill, int ex){
+		if (!enemykill && ex < 1280 && ERight){
+			ELeft = false;
+			ex += 10;
 		}
-		return px;
-	}
+		else {
+			ELeft = true;
+		}
 
-	public int getRightmove(boolean kright, int px){
-		if (kright && px < 1280){
-			px += 8;
+		if (!enemykill && ex > 0 && ELeft){
+			ERight = false;
+			ex -= 10;
 		}
-		return px;
+		else {
+			ERight = true;
+		}
+
+		if (enemykill) {
+			ex = 20;
+		}
+		return ex;
 	}
 
 	public Image getPlayer(){
 		return this.player;
+	}
+	public Image getEnemy(){
+		return this.enemy;
 	}
 }
 
